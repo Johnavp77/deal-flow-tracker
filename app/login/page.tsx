bd0a7ed54e2e77408daee547fdce23c6fde293db
Johnavp77 <johnavp77@gmail.com>
2025-08-05 16:25:28 -0700

    Create page.tsx

---
+++ app/login/page.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default function Login() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+        providers={[]}
+      />
+    </div>
+  );
+}
