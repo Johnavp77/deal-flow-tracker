5bb6a6727d85937c4722c13eb721ddb721b85fd8
Johnavp77 <johnavp77@gmail.com>
2025-08-13 14:02:26 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
 'use client';
 
 import { createClient } from '@supabase/supabase-js';
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function Page() {
-  retu
-    rn (
-    
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+        providers={['github']}
+      />
+    </div>
+  );
 }
-abc.
